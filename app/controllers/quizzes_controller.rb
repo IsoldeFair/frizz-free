daded28e487cdf82d5f39d0bb76ee2a1110db964
IsoldeFair <gosiamisiaszek1234@gmail.com>
2021-12-19 20:03:42 +0100

    add working modal with a quiz result

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,8 +1,21 @@
 class QuizzesController < ApplicationController
-  def index; end
+  def index
+    @answer = params[:answer_values] || []
+  end
 
   def count_result
-    session[:selected] = params[:post]
-    puts session[:selected]
+    answers = params[:post]
+    puts answers.values
+
+    @answer_values = Hash.new(0)
+
+    answers.values.each do |v|
+      @answer_values[v] += 1
+    end
+
+    flash[:success] = true
+    respond_to do |format|
+      format.html { redirect_to quizzes_url(answer_values: @answer_values) }
+    end
   end
 end
